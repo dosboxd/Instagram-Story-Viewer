742471958b6363daa4b2185f32335d706988f04c
Dosbol Duysekov <dosbol98@gmail.com>
2025-06-19 11:19:32 +0500

    Initial Commit

---
+++ Instagram Story Viewer/Instagram_Story_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_Story_ViewerApp.swift
+//  Instagram Story Viewer
+//
+//  Created by Dosbol on 19.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_Story_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
